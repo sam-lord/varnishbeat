e36c12346ea7d01cb47fa31e5a57cbb0dd857d57
Sam Lord <hello@samlord.me>
2020-12-04 18:02:02 +0000

    Initial commit

--- go.mod
+++ go.mod
@@ -34,6 +34,7 @@ require (
 	github.com/magefile/mage v1.10.0
 	github.com/mitchellh/gox v1.0.1
 	github.com/mitchellh/hashstructure v1.1.0 // indirect
+	github.com/phenomenes/vago v1.3.0
 	github.com/pierrre/gotestcover v0.0.0-20160517101806-924dca7d15f0
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/procfs v0.2.0 // indirect
--- module/varnish/status/status.go
+++ module/varnish/status/status.go
@@ -4,6 +4,7 @@ import (
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/common/cfgwarn"
 	"github.com/elastic/beats/v7/metricbeat/mb"
+	"github.com/phenomenes/vago"
 )
 
 // init registers the MetricSet with the central registry as soon as the program
@@ -20,7 +21,10 @@ func init() {
 // interface methods except for Fetch.
 type MetricSet struct {
 	mb.BaseMetricSet
-	counter int
+	// MetricSet needs a vago instance
+	varnish *vago.Varnish
+	// Metric properties should be contained in a dictionary so they can be dynamic
+	stats map[string]uint64
 }
 
 // New creates a new instance of the MetricSet. New is responsible for unpacking
@@ -33,9 +37,16 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 		return nil, err
 	}
 
+	// Todo pull config from module
+	vagoConfig := vago.Config{}
+	varnish, err := vago.Open(&vagoConfig)
+	if err != nil {
+		return nil, err
+	}
+
 	return &MetricSet{
 		BaseMetricSet: base,
-		counter:       1,
+		varnish:       varnish,
 	}, nil
 }
 
@@ -43,12 +54,17 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 // format. It publishes the event which is then forwarded to the output. In case
 // of an error set the Error field of mb.Event or simply call report.Error().
 func (m *MetricSet) Fetch(report mb.ReporterV2) error {
+	// Use varnish library to pull metrics, then assign them to property bag based on config
+	stats := m.varnish.Stats()
+
+	metricSetFields := common.MapStr{}
+	for field, value := range stats {
+		metricSetFields[field] = value
+	}
+
 	report.Event(mb.Event{
-		MetricSetFields: common.MapStr{
-			"counter": m.counter,
-		},
+		MetricSetFields: metricSetFields,
 	})
-	m.counter++
 
 	return nil
 }
